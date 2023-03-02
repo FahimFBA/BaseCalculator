831ca8b121ee368bc6db743969c9f8f2f1623830
FahimFBA <fahimbinamin@gmail.com>
2023-03-02 12:11:25 +0600

    Remove: HelloApplication Default: MainLanding

--- src/main/java/com/calculator/basecalculator/NextScreen.java
+++ src/main/java/com/calculator/basecalculator/NextScreen.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class NextScreen extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("nextscreen-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainLanding.class.getResource("nextscreen-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Base Conversion Calculator");
         stage.setScene(scene);
